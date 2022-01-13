d4ab1791766f2631e076a345a1f17dbe968db29e
Alexander Huerta <alexhuerta305@gmail.com>
2022-01-13 11:09:00 -0500

    improved method of skipping over fist row in csv when addedin to db

--- readFile.js
+++ readFile.js
@@ -27,31 +27,43 @@ const addProd = (csvFile) => {
     let products = [];
 
     let rows = data.split('\n');
-    rows = rows.slice(1);
+    // rows = rows.slice(1);
 
     rows.forEach((row) => {
-      let column = row.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/)
-       if(column[0] === 'id' || column[0] === NaN) column[0] = 0;
-       if(column[5] === 'default_price'|| column[0] === NaN) column[5] = '0';
-       let _id = parseInt(column[0]);
-       let _name = removeQuotes(column[1])
-       let _slogan = removeQuotes(column[2])
-       let _description = removeQuotes(column[3])
-       let _category = removeQuotes(column[4])
-       let _default_price = column[5]
 
-       let product = {
-         id: _id,
-         name: _name,
-         slogan: _slogan,
-         description: _description,
-         category: _category,
-         default_price: _default_price
-       }
-       products.push(product)
+      let column = row.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/)
+
+      // if(!readHeader) {
+      //   readHeader = true;
+      // }
+      // if(column[0] === 'id' || column[0] === NaN) column[0] = 0;
+      // if(column[5] === 'default_price'|| column[0] === NaN) column[5] = '0';
+      let _id = Number(column[0])
+      let valueCheck = isNaN(_id)
+
+      if(!valueCheck) {
+        let _name = removeQuotes(column[1])
+        let _slogan = removeQuotes(column[2])
+        let _description = removeQuotes(column[3])
+        let _category = removeQuotes(column[4])
+        let _default_price = column[5]
+        //  var default_price = column[5].replace(‘“default_price”: ‘, ‘’)
+        // let _default_price = split(column[5]).replace('"default_price": ','');
+         let product = {
+           id: _id,
+           name: _name,
+           slogan: _slogan,
+           description: _description,
+           category: _category,
+           default_price: _default_price
+         }
+         products.push(product)
+      }
+
     })
      db.addProductToDatabase(products)
   });
+
 }
 
 
